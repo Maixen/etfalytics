02a420b3ea0a0236b44b068ce4ea0a57f00e4b91
Maixen <maixen@gmx.de>
2023-07-20 17:10:52 +0200

    200723-1710

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'etfalytics.Uw0ntg3tthIskeyb3c4useItIss3cret.rn7839277462004612543834646'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://etfalytics_db_user:PTHlWrWd7Ycywgg28t9GAYU7E5rxV3ol@dpg-cisitk18g3n42onjre90-a.frankfurt-postgres.render.com/etfalytics_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     # postgres://etfalytics_db_user:PTHlWrWd7Ycywgg28t9GAYU7E5rxV3ol@dpg-cisitk18g3n42onjre90-a.frankfurt-postgres.render.com/etfalytics_db
     db.init_app(app)
 
